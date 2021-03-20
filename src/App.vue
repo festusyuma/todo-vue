a209288930f582c6ef64ed0879da008258601e00
Festus agboma <festusyuma@gmail.com>
2021-03-20 13:11:37 +0100

    delete data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-      <h1>Todos</h1>
-      <Todos :todos=todos />
-  </div>
+    <div id="app">
+        <h1>Todos</h1>
+        <Todos :todos=todos @del-todo="deleteTodo" />
+    </div>
 </template>
 
 <script>
@@ -10,31 +10,36 @@
 import Todos from './components/Todos'
 
 export default {
-  name: 'App',
-  components: {
-      Todos
-  },
-  data() {
-      return {
-          todos: [
-              {
-                  id: 1,
-                  title: 'one',
-                  completed: false
-              },
-              {
-                  id: 2,
-                  title: 'two',
-                  completed: true
-              },
-              {
-                  id: 3,
-                  title: 'three',
-                  completed: false
-              }
-          ]
-      }
-  }
+    name: 'App',
+    components: {
+        Todos
+    },
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    title: 'one',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: 'two',
+                    completed: true
+                },
+                {
+                    id: 3,
+                    title: 'three',
+                    completed: false
+                }
+            ]
+        }
+    },
+    methods: {
+        deleteTodo(id) {
+            this.todos = this.todos.filter(todo => todo.id !== id)
+        }
+    }
 }
 </script>
 
