e322bb201ed24b3412f4f0c9f4bfba3700ce8131
Festus agboma <festusyuma@gmail.com>
2021-03-21 16:57:42 +0100

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,32 @@
 <template>
-    <div id="app">
-        <Header />
-        <AddTodo @add-todo="addTodo" />
-        <Todos :todos=todos @del-todo="deleteTodo" />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-
-import Todos from './components/Todos'
-import Header from "@/components/layout/Header";
-import AddTodo from "@/components/AddTodo";
-import axios from 'axios'
-
-export default {
-    name: 'App',
-    components: {
-        Todos,
-        Header,
-        AddTodo
-    },
-    data() {
-        return {
-            todos: []
-        }
-    },
-    created() {
-        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-        .then(response => this.todos = response.data)
-        .catch(error => console.log(error))
-    },
-    methods: {
-        deleteTodo(id) {
-            axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
-            .catch(error => console.log(error))
-        },
-
-        addTodo(todo) {
-            const {title, completed} = todo
-
-            axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed})
-            .then(res => this.todos.push(res.data))
-            .catch(error => console.log(error))
-        }
-    }
-}
-</script>
-
 <style>
-*{
-    padding: 0;
-    margin: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn:hover {
-    background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
